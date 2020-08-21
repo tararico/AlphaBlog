d8d353c4a63ec862bea8ea762d372f8bcb80c75f
tararico <emipu.uq@gmail.com>
2020-08-21 14:55:09 +0900

    add pagenation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welocome to Alpha Blog #{@user.username}, you have successfully signed up"
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
       render 'new'
