c9e61b9ef4f7e89f2c04620fcbdbf3041a5cb2f8
tararico <emipu.uq@gmail.com>
2020-09-01 23:14:14 +0900

    add create controller test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -11,4 +21,9 @@ class CategoriesController < ApplicationController
   def index
 
   end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
