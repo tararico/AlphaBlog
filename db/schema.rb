8c27cf4232efdfe5afc59eed768f7b62f0510cc1
tararico <emipu.uq@gmail.com>
2020-09-08 21:48:39 +0900

    cerate article_category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_063323) do
+ActiveRecord::Schema.define(version: 2020_09_08_065903) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
